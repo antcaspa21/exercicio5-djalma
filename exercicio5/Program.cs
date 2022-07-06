4d0bf637949de193ea2435ef98f1dc1a83b527b0
silva <silva@DESKTOP-4PEQ72D>
2022-07-06 18:42:36 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //exercicio5
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("numero par:" + numero);
+                }
+                else
+                {
+                    Console.WriteLine("numero impar:" + numero);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
